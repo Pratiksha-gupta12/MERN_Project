a16ab48d8bce054d106a519a983c5e0a25b9666d
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2025-01-12 00:56:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const authRouter = require('./routes/auth/auth-routes');
 
 //create a database connection
 // we can also create a separate file for this and import that file
-mongoose.connect('mongodb+srv://pratikshag508:Pratiksha12@cluster0.sezdr.mongodb.net/').then(()=>console.log('MongoDB connected')).catch((error)=> console.log(error));
+mongoose.connect('db url').then(()=>console.log('MongoDB connected')).catch((error)=> console.log(error));
 
 const app = express()
 const PORT = process.env.PORT || 5001; //issue here
