ba3b395e3ccd4293123470e9349db02bc5e9e9de
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2025-01-24 23:23:06 +0530

    product listing

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,39 +1,191 @@
-import { adminSidebarMenuItems } from "@/config";
-import { ChartNoAxesCombined } from "lucide-react";
+/* eslint-disable react/prop-types */
+
+import {
+  BadgeCheck,
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  ShoppingBasket,
+} from "lucide-react";
+
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
-function MenuItems(){
+// import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 
-    const navigate= useNavigate();
+// eslint-disable-next-line react-refresh/only-export-components
+export const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
 
-    return <nav className="mt-8 flex-col flex gap-2">
-        {
-            adminSidebarMenuItems.map(mapItem=> <div key={menuItem.id}  onClick={()=>navigate(menuItem.path)} className="flex items-center gap-2 rounded-md px-3 py-2">
-                {MenuItem.icon}
-                <span>{MenuItem.label}</span>
+function MenuItems({setOpen}) {
+  const navigate = useNavigate();
 
-            </div>)
-        }
-
+  return (
+    <nav className="mt-8 flex-col flex gap-2">
+      {adminSidebarMenuItems.map((menuItem) => (
+        <div
+          key={menuItem.id}
+          onClick={() => {navigate(menuItem.path)
+            setOpen ? setOpen(false) : null
+          }}
+          className="flex text-xl cursor-pointer items-center gap-2 rounded-md px-3 py-2 text-muted-foreground 
+             hover:bg-muted 
+             hover:text-foreground"
+        >
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+        </div>
+      ))}
     </nav>
+  );
 }
 
+function AdminSideBar({ open, setOpen }) {
+  const navigate = useNavigate();
 
+  return (
+    <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                <ChartNoAxesCombined size={30} />
+                <h1 className="text-2xl font-extrabold">Admin Panel</h1>              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
 
-function AdminSideBar() {
-    const navigate= useNavigate();
-
-    return( <Fragment>
-        <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
-            <div onClick={()=>navigate('/admin/dashboard')} className= "flex cursor-pointer items-center gap-2">
-                <ChartNoAxesCombined  size={30}/>
-                <h1 className="text-xl font-extrabold">Admin Panel</h1>
-
-            </div>
-        </aside>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          onClick={() => navigate("/admin/dashboard")}
+          className="flex cursor-pointer items-center gap-2"
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+        </div>
+        <MenuItems />
+      </aside>
     </Fragment>
-    )
+  );
 }
 
 export default AdminSideBar;
+
+// /* eslint-disable react/prop-types */
+
+// import { ChartNoAxesCombined } from "lucide-react";
+// import { Fragment } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { LayoutDashboard, ListOrdered, ShoppingBasket } from "lucide-react"
+// // import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+
+// // eslint-disable-next-line react-refresh/only-export-components
+// export const adminSidebarMenuItems = [
+//   {
+//     id: 'dashboard',
+//     label: 'Dashboard',
+//     path: '/admin/dashboard',
+//     icons: <LayoutDashboard />,
+//   },
+//   {
+//     id: 'products',
+//     label: 'Products',
+//     path: '/admin/products',
+//     icons: <ShoppingBasket />,
+//   },
+//   {
+//     id: 'orders',
+//     label: 'Orders',
+//     path: '/admin/orders',
+//     icons: <ListOrdered />,
+//   },
+//   // {
+//   //   id: 'features',
+//   //   label: 'Features',
+//   //   path: '/admin/features',
+//   //   icons: <Anvil />,
+//   // }
+// ]
+
+// function MenuItems({ setOpen }) {
+
+//   const navigate = useNavigate();
+
+//   return (
+//     <nav className="mt-8 flex-col flex gap-2">
+//       {
+//         adminSidebarMenuItems.map((menuItem) => (
+//           <div key={menuItem.id}
+//             onClick={() => {
+//               navigate(menuItem.path)
+//               setOpen ? setOpen(false) : null;
+//             }}
+//             className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground">
+//             {menuItem.icons}
+//             <span>{menuItem.label}</span>
+//           </div>
+//         ))
+//       }
+//     </nav>
+//   );
+// }
+
+// function AdminSideBar({ open, setOpen }) {
+
+//   const navigate = useNavigate();
+
+//   return (
+
+//     <Fragment>
+
+//       {/* <Sheet open={open} onOpenChange={setOpen}>
+//         <SheetContent side="left" className="w-64">
+//           <div className="felx flex-col h-full">
+//             <SheetHeader className="border-b">
+//               <SheetTitle className="flex gap-2 mt-5 mb-5"> */}
+//                 <ChartNoAxesCombined size={30} />
+//                 <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+//               {/* </SheetTitle>
+//             </SheetHeader> */}
+//             <MenuItems setOpen={setOpen} />
+//           </div>
+//         // </SheetContent>
+
+//     //   </Sheet>
+
+//       <aside className="hidden lg:flex w-64 flex-col border-r bg-background p-6">
+//         <div className="flex cursor-pointer items-center gap-2" >
+//           <ChartNoAxesCombined size={30} />
+//           <h1 onClick={() =>
+//             navigate('/admin/dashboard')}
+//             className="text-2xl font-extrabold">Admin Panel</h1>
+
+//         </div>
+//         <MenuItems />
+//       </aside>
+//     </Fragment>
+//   );
+// };
+
+// export default AdminSideBar;
--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Fragment } from "react";
+
 function AdminProducts(){
     return(
-        <div>admin products</div>
+        <Fragment>
+            <div className="mb-5 w-full flex justify-end">
+                <Button>Add New Product</Button>
+            </div>
+            <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+        </Fragment>
     )
 };
 
