a81e4baf5613d44b15bbd764f6a4b118e6521562
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2025-05-07 23:59:09 +0530

    Update order-controller.js

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -1,4 +1,6 @@
-const { redirect } = require("react-router-dom");
+// const { redirect } = require("react-router-dom");
+const paypal = require('../../helpers/paypal')
+const Order = require('../../models/Order')
 
 
 
@@ -25,10 +27,59 @@ const createOrder = async(req , res)=>{
                 payment_method :'paypal'
             },
             redirect_urls : {
-                return_url :,
-                cancel_url : 
+                return_url : 'http://localhost:5173/shop/paypal-return',
+                cancel_url : 'http://localhost:5173/shop/paypal-cancel'
+            },
+            transactions : [
+                {
+                    item_list : {
+                        items : cartItems.map(item => ({
+                            name : item.title,
+                            sku : item.productId,
+                            price: item.price.toFixed(2),
+                            currency : 'USD',
+                            quantity : item.quantity
+                        }))
+                    },
+                    amount : {
+                        currency : 'USD',
+                        total : totalAmount.toFixed(2)
+                    },
+                    description : 'description'
+                }
+            ]
+        };
+
+
+
+        paypal.payment.create(create_payment_json, async(error, paymentInfo)=>{
+            if(error){
+                console.log(error)
+
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error while creating paypal payment'
+                })
+            } else{
+                const newlyCreatedOrder = new Order({
+                    userId, cartItems, addressInfo,
+            orderStatus ,
+  paymentMethod,
+  paymentStatus,
+  totalAmount,
+  orderDate,
+  orderUpdateDate,
+  paymentId,
+  payerId,
+                })
+
+                await newlyCreatedOrder.save();
+
+                const approvalURL = paymentInfo.links.find(link => link.rel === 'approval_url').href
+
+                
             }
-        }
+        })
 
     }catch(e){
         console.log(e);
